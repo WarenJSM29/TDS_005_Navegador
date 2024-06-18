3e1f7ae2e3aada708d16d05fc9a0038017ee60d8
WarenJSM29 <warrensm87@gmail.com>
2024-06-18 01:36:20 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace TDS_005_Navegador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAtras = new System.Windows.Forms.ToolStripButton();
+            this.btnAdelante = new System.Windows.Forms.ToolStripButton();
+            this.btnActualizar = new System.Windows.Forms.ToolStripButton();
+            this.txtBuscador = new System.Windows.Forms.ToolStripTextBox();
+            this.cmbFavoritos = new System.Windows.Forms.ToolStripComboBox();
+            this.btnFavoritos = new System.Windows.Forms.ToolStripButton();
+            this.btnHogar = new System.Windows.Forms.ToolStripButton();
+            this.btnOscuroClaro = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(887, 614);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webView2);
+            this.tabPage1.Controls.Add(this.toolStrip1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(879, 588);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webView2
+            // 
+            this.webView2.AllowExternalDrop = true;
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Location = new System.Drawing.Point(0, 31);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(879, 561);
+            this.webView2.TabIndex = 1;
+            this.webView2.ZoomFactor = 1D;
+            this.webView2.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView2_NavigationCompleted);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAtras,
+            this.btnAdelante,
+            this.btnActualizar,
+            this.txtBuscador,
+            this.cmbFavoritos,
+            this.btnFavoritos,
+            this.btnHogar,
+            this.btnOscuroClaro});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 3);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(873, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.AutoSize = false;
+            this.btnAtras.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAtras.Image = global::TDS_005_Navegador.Properties.Resources.flecha_cuadrado_izquierda_modo_claro;
+            this.btnAtras.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(23, 22);
+            this.btnAtras.Text = "toolStripButton1";
+            this.btnAtras.ToolTipText = "Atras";
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnAdelante
+            // 
+            this.btnAdelante.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAdelante.Image = global::TDS_005_Navegador.Properties.Resources.flecha_cuadrado_derecha_modo_claro;
+            this.btnAdelante.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdelante.Name = "btnAdelante";
+            this.btnAdelante.Size = new System.Drawing.Size(23, 22);
+            this.btnAdelante.Text = "Avanzar";
+            this.btnAdelante.Click += new System.EventHandler(this.btnAdelante_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnActualizar.Image = global::TDS_005_Navegador.Properties.Resources.girar_cuadrado_modo_claro;
+            this.btnActualizar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(23, 22);
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // txtBuscador
+            // 
+            this.txtBuscador.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtBuscador.Name = "txtBuscador";
+            this.txtBuscador.Size = new System.Drawing.Size(600, 25);
+            this.txtBuscador.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBuscador_KeyDown);
+            // 
+            // cmbFavoritos
+            // 
+            this.cmbFavoritos.Name = "cmbFavoritos";
+            this.cmbFavoritos.Size = new System.Drawing.Size(121, 25);
+            this.cmbFavoritos.SelectedIndexChanged += new System.EventHandler(this.cmbFavoritos_SelectedIndexChanged);
+            // 
+            // btnFavoritos
+            // 
+            this.btnFavoritos.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnFavoritos.Image = global::TDS_005_Navegador.Properties.Resources.estrella_de_la_lista_de_deseos_modo_claro;
+            this.btnFavoritos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFavoritos.Name = "btnFavoritos";
+            this.btnFavoritos.Size = new System.Drawing.Size(23, 22);
+            this.btnFavoritos.Text = "Favoritos";
+            this.btnFavoritos.Click += new System.EventHandler(this.btnFavoritos_Click);
+            // 
+            // btnHogar
+            // 
+            this.btnHogar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnHogar.Image = global::TDS_005_Navegador.Properties.Resources.hogar_modo_claro;
+            this.btnHogar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnHogar.Name = "btnHogar";
+            this.btnHogar.Size = new System.Drawing.Size(23, 22);
+            this.btnHogar.Text = "Hogar";
+            this.btnHogar.Click += new System.EventHandler(this.btnHogar_Click);
+            // 
+            // btnOscuroClaro
+            // 
+            this.btnOscuroClaro.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnOscuroClaro.Image = global::TDS_005_Navegador.Properties.Resources.eclipse_alt_modo_claro;
+            this.btnOscuroClaro.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOscuroClaro.Name = "btnOscuroClaro";
+            this.btnOscuroClaro.Size = new System.Drawing.Size(23, 22);
+            this.btnOscuroClaro.Text = "Cambiar Modo Oscuro y Claro";
+            this.btnOscuroClaro.Click += new System.EventHandler(this.btnOscuroClaro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 614);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Waren";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAtras;
+        private System.Windows.Forms.ToolStripButton btnAdelante;
+        private System.Windows.Forms.ToolStripButton btnActualizar;
+        private System.Windows.Forms.ToolStripButton btnFavoritos;
+        private System.Windows.Forms.ToolStripTextBox txtBuscador;
+        private System.Windows.Forms.ToolStripComboBox cmbFavoritos;
+        private System.Windows.Forms.ToolStripButton btnHogar;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+        private System.Windows.Forms.ToolStripButton btnOscuroClaro;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+
+// Nombre del Autor: Waren Jesús Sánchez Madé
+// Matrícula: 2023-1198
+
+namespace TDS_005_Navegador
+{
+    public partial class Form1 : Form
+    {
+        // Variable para rastrear el estado del modo oscuro
+        private bool EsModoOscuro = false;
+
+        // Variable para rastrear si el navegador está actualmente refrescando la página.
+        private bool estaActualizando = false;
+
+        // Lista para almacenar las URLs favoritas.
+        List<string> Favoritos = new List<string>();
+
+        // Constructor del formulario.
+        public Form1()
+        {
+            InitializeComponent(); // Inicializa los componentes del formulario.
+            InitializeWebView(); // Inicializa el componente WebView2.
+            ConfigurarModoClaro(); // Inicializa con modo claro por defecto
+        }
+
+        // Método asincrónico para inicializar WebView2.
+        private async void InitializeWebView()
+        {
+            // Comprueba que webView2 esté correctamente inicializado en el diseñador.
+            if (webView2 != null)
+            {
+                // Inicializa el componente WebView2.
+                await webView2.EnsureCoreWebView2Async(null);
+                // Asigna el evento de navegación completada.
+                webView2.CoreWebView2.NavigationCompleted += webView2_NavigationCompleted;
+
+                // Navega a la página web predeterminada.
+                webView2.CoreWebView2.Navigate("https://www.bing.com/");
+            }
+        }
+
+        // Evento click para el botón 'Atrás' que navega a la página anterior en el historial del navegador.
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            if (webView2.CanGoBack)
+            {
+                webView2.GoBack(); // Navega a la página anterior.
+            }
+        }
+
+        // Evento click para el botón 'Adelante' que navega a la página siguiente en el historial del navegador.
+        private void btnAdelante_Click(object sender, EventArgs e)
+        {
+            if (webView2.CanGoForward)
+            {
+                webView2.GoForward(); // Navega a la página siguiente.
+            }
+        }
+
+        // Evento click para el botón 'Actualizar' que refresca o detiene la actualización de la página.
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (!estaActualizando)
+            {
+                webView2.Reload(); // Refresca la página.
+                estaActualizando = true; // Establece la variable de actualización a verdadero.
+            }
+            else
+            {
+                webView2.CoreWebView2.Stop(); // Detiene la actualización de la página.
+                estaActualizando = false; // Establece la bandera de actualización a falso.
+            }
+        }
+
+        // Evento KeyDown para el cuadro de texto 'Buscador' que navega a la URL ingresada al presionar 'Enter'.
+        private void txtBuscador_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) // Comprueba si la tecla presionada es Enter.
+            {
+                if (webView2 != null && webView2.CoreWebView2 != null)
+                {
+                    string url = txtBuscador.Text.Trim(); // Elimina espacios en blanco al principio y al final
+
+                    // Verifica si la URL es válida
+                    if (EsLaUrlValida(url))
+                    {
+                        webView2.CoreWebView2.Navigate(url); // Navega a la URL ingresada
+                    }
+                    else
+                    {
+                        // Si la URL no es válida, intenta agregar 'http://' al principio
+                        url = "http://" + url;
+
+                        if (EsLaUrlValida(url))
+                        {
+                            webView2.CoreWebView2.Navigate(url); // Navega a la URL ingresada con 'http://'
+                        }
+                        else
+                        {
+                            MessageBox.Show("Por favor, ingrese una URL válida que termine con '.com'", "URL no válida",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning); // Muestra un mensaje de advertencia si la URL no es válida.
+                        }
+                    }
+                }
+                e.Handled = true; // Previene el sonido de error de Windows.
+            }
+        }
+
+        // Método para verificar si la URL es válida.
+        private bool EsLaUrlValida(string url)
+        {
+            // Comprueba si la cadena es una URL con formato correcto.
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                return false; // Devuelve falso si la URL no tiene un formato correcto.
+            }
+
+            // Intenta crear una Uri a partir de la cadena y comprueba si tiene un esquema HTTP o HTTPS.
+            Uri uriResult;
+            bool result = Uri.TryCreate(url, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            return result; // Devuelve verdadero si la URL es válida y tiene un esquema HTTP o HTTPS.
+        }
+
+        // Método para actualizar el ComboBox con las URLs favoritas.
+        private void actualizarCmbBox()
+        {
+            cmbFavoritos.Items.Clear(); // Limpia los items existentes para evitar duplicados.
+            foreach (string direccion in Favoritos)
+            {
+                cmbFavoritos.Items.Add(direccion); // Agrega cada URL favorita al ComboBox.
+            }
+        }
+
+        // Evento click para el botón 'Favoritos' que agrega la URL actual a la lista de favoritos y actualiza el ComboBox.
+        private void btnFavoritos_Click(object sender, EventArgs e)
+        {
+            Favoritos.Add(webView2.Source.ToString()); // Agrega la URL actual a la lista de favoritos.
+            actualizarCmbBox(); // Actualiza el ComboBox de favoritos.
+        }
+
+        // Evento que se dispara al seleccionar un item del ComboBox 'Favoritos', navegando a la URL seleccionada.
+        private void cmbFavoritos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            webView2.CoreWebView2.Navigate(cmbFavoritos.SelectedItem.ToString()); // Navega a la URL seleccionada.
+        }
+
+        // Evento click para el botón 'Hogar' que navega a la página de inicio del navegador.
+        private void btnHogar_Click(object sender, EventArgs e)
+        {
+            webView2.CoreWebView2.Navigate("https://www.bing.com/"); // Navega a la página de inicio.
+        }
+
+        // Evento que se dispara cuando una página termina de cargar, restableciendo la variable 'estaActualizando'.
+        private void webView2_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            estaActualizando = false; // Restablece la variable de actualización a falso.
+        }
+
+        private void btnOscuroClaro_Click(object sender, EventArgs e)
+        {
+            if (EsModoOscuro)
+            {
+                ConfigurarModoClaro();
+            }
+            else
+            {
+                ConfigurarModoOscuro();
+            }
+
+            EsModoOscuro = !EsModoOscuro;
+        }
+
+        private void ConfigurarModoClaro()
+        {
+            this.BackColor = Color.White;
+            this.ForeColor = Color.Black;
+            toolStrip1.BackColor = Color.LightGray;
+            toolStrip1.ForeColor = Color.Black;
+            tabControl1.BackColor = Color.White;
+            tabControl1.ForeColor = Color.Black;
+            foreach (TabPage tabPage in tabControl1.TabPages)
+            {
+                tabPage.BackColor = Color.White;
+                tabPage.ForeColor = Color.Black;
+            }
+            tabPage1.BackColor = Color.White;
+            tabPage1.ForeColor = Color.Black;
+            webView2.DefaultBackgroundColor = Color.White;
+
+            foreach (ToolStripItem item in toolStrip1.Items)
+            {
+                item.BackColor = Color.LightGray;
+                item.ForeColor = Color.Black;
+            }
+
+            // Cambiar color de fondo y texto para el ToolStripTextBox y ToolStripComboBox
+            txtBuscador.BackColor = Color.White;
+            txtBuscador.ForeColor = Color.Black;
+            cmbFavoritos.BackColor = Color.White;
+            cmbFavoritos.ForeColor = Color.Black;
+
+            // Cambiar íconos a modo claro
+            btnAtras.Image = Properties.Resources.flecha_cuadrado_izquierda_modo_claro;
+            btnAdelante.Image = Properties.Resources.flecha_cuadrado_derecha_modo_claro;
+            btnActualizar.Image = Properties.Resources.girar_cuadrado_modo_claro;
+            btnHogar.Image = Properties.Resources.hogar_modo_claro;
+            btnFavoritos.Image = Properties.Resources.estrella_de_la_lista_de_deseos_modo_claro;
+            btnOscuroClaro.Image = Properties.Resources.eclipse_alt_modo_claro;
+        }
+
+        private void ConfigurarModoOscuro()
+        {
+            this.BackColor = Color.FromArgb(45, 45, 48);
+            this.ForeColor = Color.White;
+            toolStrip1.BackColor = Color.FromArgb(28, 28, 28);
+            toolStrip1.ForeColor = Color.White;
+            tabControl1.BackColor = Color.FromArgb(45, 45, 48);
+            tabControl1.ForeColor = Color.White;
+            foreach (TabPage tabPage in tabControl1.TabPages)
+            {
+                tabPage.BackColor = Color.FromArgb(45, 45, 48);
+                tabPage.ForeColor = Color.White;
+            }
+            tabPage1.BackColor = Color.FromArgb(45, 45, 48);
+            tabPage1.ForeColor = Color.White;
+            webView2.DefaultBackgroundColor = Color.FromArgb(45, 45, 48);
+
+            foreach (ToolStripItem item in toolStrip1.Items)
+            {
+                item.BackColor = Color.FromArgb(28, 28, 28);
+                item.ForeColor = Color.White;
+            }
+
+            // Cambiar color de fondo y texto para el ToolStripTextBox y ToolStripComboBox
+            txtBuscador.BackColor = Color.FromArgb(60, 60, 64);
+            txtBuscador.ForeColor = Color.White;
+            cmbFavoritos.BackColor = Color.FromArgb(60, 60, 64);
+            cmbFavoritos.ForeColor = Color.White;
+
+            // Cambiar íconos a modo oscuro
+            btnAtras.Image = Properties.Resources.flecha_cuadrado_izquierda_modo_oscuro;
+            btnAdelante.Image = Properties.Resources.flecha_cuadrado_derecha_modo_oscuro;
+            btnActualizar.Image = Properties.Resources.girar_cuadrado_modo_oscuro;
+            btnHogar.Image = Properties.Resources.hogar_modo_oscuro;
+            btnFavoritos.Image = Properties.Resources.estrella_de_la_lista_de_deseos_modo_oscuro;
+            btnOscuroClaro.Image = Properties.Resources.eclipse_alt_modo_oscuro;
+        }
+    }
+}
